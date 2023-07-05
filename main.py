45fd1ac7e961e7372c9dc3a30e808a57da7d2b8c
111 <sin.leonid@gmail.com>
2023-07-05 19:36:42 +0300

    hello

---
+++ main.py
@@ -0,0 +1,21 @@
+
+# #singer, name = map(str, input().split())
+
+singer = 'eisbrecher'
+name = 'angst'
+
+import requests
+from bs4 import BeautifulSoup
+
+
+response = requests.get(f'https://www.amalgama-lab.com/songs/{singer[0]}/{singer}/{name}.html')
+
+soup = BeautifulSoup(response.text, 'lxml')
+
+
+original = soup.find_all('div', class_='original')
+translate = soup.find_all('div', class_='translate')
+
+for i in range(len(original)):
+    print(original[i].text)
+    print(translate[i].text)
